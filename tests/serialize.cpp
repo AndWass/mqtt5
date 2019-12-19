902820e6e05bb67931026f5a3dcff6f6fd008e2d
Andreas Wass <wass.andreas@gmail.com>
2019-12-20 00:10:17 +0100

    Add test to count length of variable length integer

--- tests/serialize.cpp
+++ tests/serialize.cpp
@@ -1,22 +1,28 @@
-#include <doctest/doctest.h>
 #include <algorithm>
+#include <doctest/doctest.h>
 
-#include <string>
+#include "mqtt5/integer.hpp"
 #include "mqtt5/serialize.hpp"
 #include "mqtt5/string.hpp"
+#include <string>
 
-TEST_CASE("count_iterator: std::copy can count")
-{
+TEST_CASE("count_iterator: std::copy can count") {
     mqtt5::count_iterator iter;
     std::string str("hello world");
     iter = std::copy(str.begin(), str.end(), iter);
     REQUIRE(iter.count == str.size());
 }
 
-TEST_CASE("string: serialize UTF string")
-{
+TEST_CASE("count_iterator: count length of UTF string") {
     mqtt5::string str("A\xF0\xAA\x9B\x94");
     mqtt5::count_iterator iter;
     iter = serialize(str, iter);
-    REQUIRE(iter.count == 7);    
+    REQUIRE(iter.count == 7);
+}
+
+TEST_CASE("count_iterator: count length of variable length integer") {
+    mqtt5::varlen_integer vi(100000000);
+    mqtt5::count_iterator iter;
+    iter = serialize(vi, iter);
+    REQUIRE(iter.count == 4);
 }
