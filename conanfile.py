631b1aa319edb7a217b22ede78f9d08a2c502ed5
Andreas Wass <wass.andreas@gmail.com>
2020-04-02 18:08:32 +0200

    Cleanup and fixes

--- conanfile.py
+++ conanfile.py
@@ -4,13 +4,21 @@ class Mqtt5(ConanFile):
     name = "mqtt5"
     version = "1.0.0"
     generators = "cmake"
-    options = {"build_tests": [True, False]}
-    default_options = {"build_tests": False}
+    options = {
+        "build_tests": [True, False],
+        "ssl": [True, False]
+        }
+    default_options = {
+        "build_tests": False,
+        "ssl": True
+        }
     requires = ("span-lite/0.7.0",
-        "boost/[>=1.71]@conan/stable",
+        "boost/[>=1.72]",
         "p0443/0.0.4k"
         )
 
     def requirements(self):
         if self.options.build_tests:
             self.requires("doctest/2.3.5")
+        if self.options.ssl:
+            self.requires("openssl/1.1.1f")
