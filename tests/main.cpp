bea0e5b4e3aef7ca2afad3452c6b8901f932b5b4
Andreas Wass <andreas.wass@dalelven.com>
2020-04-02 08:19:24 +0200

    Add missing license headers

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,2 +1,8 @@
+
+//          Copyright Andreas Wass 2004 - 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
