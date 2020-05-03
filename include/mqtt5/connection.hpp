fe47dde65397a525b777faf944f63edc101d6ebc
Andreas Wass <wass.andreas@gmail.com>
2020-05-03 17:23:24 +0200

    Add a basic comment on mqtt5/connection

--- include/mqtt5/connection.hpp
+++ include/mqtt5/connection.hpp
@@ -46,6 +46,12 @@ auto& get_lowest_layer(T& t) {
     }
 }
 } // namespace detail
+/**
+ * @brief An MQTT5 connection object.
+ *
+ * The connection holds an internal object of type AsyncStream
+ * which is used to read and write control messages.
+ */
 template <class AsyncStream>
 class connection
 {
@@ -57,10 +63,18 @@ public:
     using next_layer_type = typename std::remove_reference_t<AsyncStream>;
     using executor_type = typename next_layer_type::executor_type;
 
+    /**
+     * @brief Construct a connection.
+     *
+     * All arguments are forwarded to the underlying stream.
+     */
     template <class... Args>
     connection(Args &&... args) : stream_(std::forward<Args>(args)...) {
     }
 
+    /**
+     * @brief Get a reference to the next stream layer
+     */
     next_layer_type &next_layer() {
         return stream_;
     }
@@ -68,6 +82,12 @@ public:
         return stream_;
     }
 
+    /**
+     * @brief Get a reference to the lowest layer socket object.
+     *
+     * This is usually a TCP/IP socket, but can be wrapped up in
+     * various number of layers.
+     */
     auto& lowest_layer() {
         return detail::get_lowest_layer(stream_);
     }
@@ -80,6 +100,16 @@ public:
         return stream_.get_executor();
     }
 
+    /**
+     * @brief Create a reader for a complete control packet.
+     *
+     * When the reader is started it will read an entire packet from
+     * a stream.
+     *
+     * Sender value: mqtt5::protocol::control_packet
+     * Sender error: std::exception_ptr
+     * Sender sets done: yes
+     */
     auto control_packet_reader() {
         return p0443_v2::with(
             [this](protocol::control_packet &packet) {
@@ -91,6 +121,16 @@ public:
             protocol::control_packet{});
     }
 
+    /**
+     * @brief Create a reader for a complete specific control packet.
+     *
+     * When the reader is started it will read an entire packet from
+     * the connection and attemt to transform it into the specified packet.
+     *
+     * Sender value: std::optional<T>
+     * Sender error: std::exception_ptr
+     * Sender sets done: yes
+     */
     template <class T>
     auto packet_reader() {
         return p0443_v2::transform(control_packet_reader(), [](auto &&p) -> std::optional<T> {
@@ -98,6 +138,16 @@ public:
         });
     }
 
+    /**
+     * @brief Create a writer for a control packet.
+     *
+     * When the writer is started it will write the control packet to the
+     * stream.
+     *
+     * Sender value: std::size_t
+     * Sender error: std::exception_ptr
+     * Sender sets done: yes
+     */
     auto control_packet_writer(protocol::control_packet packet) {
         std::vector<std::uint8_t> buffer;
         packet.serialize([&](auto b) { buffer.push_back(b); });
